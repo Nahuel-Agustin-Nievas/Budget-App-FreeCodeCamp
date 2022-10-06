b6ae9eabe7e3c6dc1c100f6924acd1f4cd928877
Nahuel Nievas <nievasnahuel@gmail.com>
2022-10-07 00:18:05 +0200

    Budget object print done

--- ej3.py
+++ ej3.py
@@ -5,13 +5,13 @@ class Category:
       
 
     def deposit (self, amount, description = ""):
-        self.amount = amount
-        self.description = description
         self.ledger.append ({"amount": amount, "description" : description})
 
-#    def withdraw (self, amount, description = ""):
-#         self.amount = amount
-#         self.description = description
+    def withdraw (self, amount, description = ""):
+        if(self.check_funds(amount)):
+          self.ledger.append ({"amount": -amount, "description" : description})
+          return True
+        return False
 
     def transfer(self, amount, category):
         if(self.check_funds(amount)):
@@ -20,18 +20,38 @@ class Category:
             return True
         return False
 
-    def get_balance(self)
+    def get_balance(self):
 
-    totalCash= 0
-    for item in self.ledger:
-        totalCash += item["amount"]
+        totalCash= 0
+        for item in self.ledger:
+          totalCash += item["amount"]
 
-    return totalCash
+        return totalCash
 
     def check_funds(self, amount):
-        if(self.get.balance()>= amount)
+        if(self.get_balance() >= amount):
           return True
-    return False
-        
+        return False
 
 
+    def __str__(self): 
+        title = f"{self.name:*^30}\n"
+        items = ""
+        total = 0
+        for item in self.ledger:
+            items += f"{item['description'][0:23]:23}" + f"{item['amount']:>7.2f}" + '\n'
+            total += item['amount']
+
+        output = title + items + "Total: " + str(total)
+        return output
+ 
+food = Category("Food")
+food.deposit(1000, "initial deposit")
+food.withdraw(10.15, "groceries")
+food.withdraw(15.89, "restaurant and more food for dessert")
+print(food.get_balance())
+
+
+
+
+print(food)
